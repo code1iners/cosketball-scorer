bc926b529a15bed53d1a67437d0e4eb4b5dca767
Codeliner <codeliner96@gmail.com>
2021-12-12 22:27:44 +0900

    Implement app loading.

--- App.js
+++ App.js
@@ -1,10 +1,57 @@
 import { NavigationContainer } from "@react-navigation/native";
-import React from "react";
+import React, { useState } from "react";
 import RootNavigation from "./src/navigators/RootNavigator";
+import AppLoading from "expo-app-loading";
+import { Ionicons } from "@expo/vector-icons";
+import { Text } from "react-native";
+import { loadFonts } from "@ce1pers/use-resource";
 
 export default function App() {
+  const [ready, setReady] = useState(false);
+
+  // Methods.
+
+  /**
+   * ### Load resources (assets, fonts).
+   */
+  const loadResources = async () => {
+    const fonts = loadFonts([Ionicons.font]);
+    await Promise.all([...fonts]);
+  };
+
+  // Handlers.
+
+  /**
+   * ### On app loading start event handler.
+   */
+  const startAsync = async () => {
+    await loadResources();
+  };
+
+  /**
+   * ### On app loading finish event handler.
+   */
+  const onFinish = () => setReady(true);
+
+  /**
+   * ### On app loading error event handler.
+   */
+  const onError = (error) => console.error(error);
+
+  if (!ready) {
+    return (
+      <AppLoading
+        startAsync={startAsync}
+        onError={onError}
+        onFinish={onFinish}
+      />
+    );
+  }
+
   return (
     <NavigationContainer>
+      <Ionicons name="beer" size={20} />
+      <Text>asdf</Text>
       <RootNavigation />
     </NavigationContainer>
   );
--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,16 +1,6 @@
-import React, { useEffect } from "react";
+import React from "react";
 import styled from "styled-components/native";
-import { View, Text, Alert } from "react-native";
-import {
-  addListener,
-  getCurrentScreenOrientation,
-  LANDSCAPE_LEFT,
-  LANDSCAPE_RIGHT,
-  releaseOrientation,
-  removeListener,
-  setOrientationByLandscape,
-  setOrientationByPortrait,
-} from "@ce1pers/use-screen";
+import { View, Text } from "react-native";
 
 const Container = styled.View``;
 
